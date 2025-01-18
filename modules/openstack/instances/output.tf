fdfb7b5d1c8f0de086504b59b8ac185a305e387b
ngodat0103 <ngovuminhdat@gmail.com>
2025-01-18 23:40:34 +0700

    Delete uncessary file

--- modules/openstack/instances/output.tf
+++
@@ -1,6 +0,0 @@
-output "karina-internal-ip" {
-  value = openstack_compute_instance_v2.karina_instance.network[0].fixed_ip_v4
-}
-output "karina-floating-ip" {
-  value = openstack_networking_floatingip_v2.floating_ip.address
-}
--- modules/openstack/network/security/output.tf
+++
@@ -1,3 +0,0 @@
-output "security_group_name" {
-  value = openstack_networking_secgroup_v2.default_security_group.name
-}
